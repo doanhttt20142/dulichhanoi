bd6cd0ec271f466f13e39ddefb26b210cd33b46b
KiemVM <manhkiem41@gmail.com>
2015-05-24 16:24:12 +0700

    Tạo client app mudule giai tri

--- clientapp/giaitri/detailgiaitriCtrl.js
+++ clientapp/giaitri/detailgiaitriCtrl.js
@@ -2,5 +2,25 @@
  *
  */
 dulichapp.controller('detailgiaitriCtrl', function($scope) {
+	$scope.id = $routeParams.id;
 	
+	$scope.folderimgs = FOLDER_IMGS + 'giaitri/';
+	
+	$scope.giaitri = null;
+	
+	$scope.result = null;
+	
+	$scope.basicUsageOptions = {
+			$AutoPlay: true
+	};
+	
+	$scope.refresh = function() {
+		$http.get(HOST_SERVER + 'crud/detail/giaitri/' + $scope.id).success(function(data, status, headers, config) {
+			$scope.giaitri = data;
+			$scope.giaitri.mo_ta = JSON.parse($scope.giaitri.mo_ta);
+			$scope.giaitri.album = JSON.parse($scope.giaitri.album);
+		});
+	};
+	
+	$scope.refresh();
 });
