f8f73666a3860b68a9d9add15484677b8963efc5
tuantri <nguyen.tuan.tri.1993@gmail.com>
2015-06-13 09:19:17 +0700

    nothing2

--- clientapp/monan/topmonanCtrl.js
+++ clientapp/monan/topmonanCtrl.js
@@ -1,51 +1,6 @@
 /**
- * 
+ *
  */
-dulichapp.controller('topmonanCtrl', function($scope, $http) {
-	D = 5;
-	column = 'viewcount';
-	$scope.countRecord;
-	$scope.data = [];
+dulichapp.controller('topmonanCtrl', function($scope) {
 	
-	$scope.hasMore = true;
-	
-	$scope.folderimgs = FOLDER_IMGS + 'monan/';
-	
-	$scope.loadmore = function(){
-		$http.get(HOST_SERVER + 'crud/top/monan/' + column + '/' + $scope.data.length +'/' + D).success(function(data, status, headers, config) {
-			$scope.data = $scope.data.concat(data);
-			if ($scope.data.length >= $scope.countRecord)
-				$scope.hasMore = false;
-			else
-				$scope.hasMore = true;
-		});
-	};
-	
-	$scope.getCountRecord = function() {
-		$http.get(HOST_SERVER + 'crud/countRecord/monan/').success(function(data, status, headers, config) {
-			$scope.countRecord = data;
-		});
-	};
-	
-	$scope.rateCollumn = function() {
-		if (column == 'rate')
-			return;
-		column = 'rate';
-		$scope.data = [];
-		$scope.getCountRecord();
-		$scope.loadmore();
-	};
-	
-	$scope.viewCollumn = function() {
-		if (column == 'viewcount')
-			return;
-		column = 'viewcount';
-		$scope.data = [];
-		$scope.getCountRecord();
-		$scope.loadmore();
-	};
-	
-	$scope.getCountRecord();
-	
-	$scope.loadmore();
 });
